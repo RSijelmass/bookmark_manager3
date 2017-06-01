293edba437dd0b92b18ad67ca3aff419ded6e9a5
charliefea <charlotte.feather@yahoo.com>
2017-06-01 14:58:49 +0100

    Add Tag class

--- app/app.rb
+++ app/app.rb
@@ -1,16 +1,19 @@
 ENV["RACK_ENV"] ||= "development"
 require 'sinatra/base'
 require_relative './models/link.rb'
+require_relative './models/tag.rb'
 
 class BookmarkManager < Sinatra::Base
 
   get ('/links') do
     @links = Link.all
+    @tags = Tag.all
     erb :links
   end
 
   post ('/links') do
-    Link.create(title: params[:title], url: params[:url], tags: params[:tags])
+    Link.create(title: params[:title], url: params[:url])
+    Tag.create(name: params[:tags])
     redirect '/links'
   end
 
